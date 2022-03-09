804a0adb875cbe6aed36e96980f470b6436e60d1
Miguel López <lop18028@byui.edu>
2022-03-09 10:42:52 -0700

    Code for the ControlActorsAction class.
    Added methods and functions for the ControlActorsAction class.

---
+++ cycle/game/scripting/control_actors_action.py
@@ -0,0 +1,94 @@
+import constants
+from game.scripting.action import Action
+from game.shared.point import Point
+
+
+class ControlActorsAction( Action ):
+    """
+    An input action that controls the snake.
+    
+    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+
+    Attributes:
+        _keyboard_service ( KeyboardService ): An instance of KeyboardService.
+    """
+
+    def __init__( self, keyboard_service ):
+        """Constructs a new ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service ( KeyboardService ): An instance of KeyboardService.
+        """
+        self._keyboard_service = keyboard_service
+        self._direction = Point( constants.CELL_SIZE, 0 )
+        self._direction2 = Point( constants.CELL_SIZE, 0 )
+
+    def execute( self, cast, script ):
+        """Executes the control actors action.
+
+        Args:
+            cast ( Cast ): The cast of Actors in the game.
+            script ( Script ): The script of Actions in the game.
+        """
+        game_over_flag = cast.get_actors( "messages" )
+        player_one = cast.get_first_actor( "player_one" )
+        player_two = cast.get_first_actor( "player_two" )
+        player_one_score = cast.get_first_actor( "player_one_score" )
+        player_two_score = cast.get_first_actor( "player_two_score" )
+
+        # Left.
+        if self._keyboard_service.is_key_down( "a" ):
+            self._direction = Point( -constants.CELL_SIZE, 0 )
+            if not game_over_flag:
+                player_one_score.add_points( 1 )
+                player_one.grow_tail( 1 )
+
+        if self._keyboard_service.is_key_down( "j" ):
+            self._direction2 = Point( -constants.CELL_SIZE, 0 )
+            if not game_over_flag:
+                player_two_score.add_points( 1 )
+                player_two.grow_tail( 1 )
+        
+        # Right.
+        if self._keyboard_service.is_key_down( "d" ):
+            self._direction = Point( constants.CELL_SIZE, 0 )
+            if not game_over_flag:
+                player_one_score.add_points( 1 )
+                player_one.grow_tail( 1 )
+        
+        if self._keyboard_service.is_key_down( "l" ):
+            self._direction2 = Point( constants.CELL_SIZE, 0 )
+            if not game_over_flag:
+                player_two_score.add_points( 1 )
+                player_two.grow_tail( 1 )
+        
+        # Up.
+        if self._keyboard_service.is_key_down( "w" ):
+            self._direction = Point( 0, -constants.CELL_SIZE )
+            if not game_over_flag:
+                player_one_score.add_points( 1 )
+                player_one.grow_tail( 1 )
+        
+        if self._keyboard_service.is_key_down( "i" ):
+            self._direction2 = Point( 0, -constants.CELL_SIZE )
+            if not game_over_flag:
+                player_two_score.add_points( 1 )
+                player_two.grow_tail( 1 )
+        
+        # Down.
+        if self._keyboard_service.is_key_down( "s" ):
+            self._direction = Point( 0, constants.CELL_SIZE )
+            if not game_over_flag:
+                player_one_score.add_points( 1 )
+                player_one.grow_tail( 1 )
+        
+        if self._keyboard_service.is_key_down( "k" ):
+            self._direction2 = Point( 0, constants.CELL_SIZE )
+            if not game_over_flag:
+                player_two_score.add_points( 1 )
+                player_two.grow_tail( 1 )
+        
+        player_one = cast.get_first_actor( "player_one" )
+        player_two = cast.get_first_actor( "player_two" )
+        player_one.turn_head( self._direction )
+        player_two.turn_head( self._direction2 )
