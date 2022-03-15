79b00e5005ca3741ab101fd9e05567326f1f9d2b
Miguel López <lop18028@byui.edu>
2022-03-15 07:21:57 -0600

    Edit __main__.py file.
    Added the call for the Tail class.

--- cycle/__main__.py
+++ cycle/__main__.py
@@ -1,7 +1,7 @@
 from game.casting.cast import Cast
-from game.casting.player_one_score import Player_one_score
-from game.casting.player_two_score import Player_two_score
+from game.casting.score import Score
 from game.casting.cycle import Cycle
+from game.casting.tail import Tail
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
@@ -17,9 +17,15 @@ def main():
     # Create the cast.
     cast = Cast()
     cast.add_actor( "player_one", Cycle( color = constants.GREEN, position = 550 ) )
+    player_one = cast.get_first_actor( "player_one" )
+    tail_one = player_one.get_segments()
+    cast.add_actor( "player_one_tail", Tail( color = constants.GREEN, player_segments = tail_one ) )
     cast.add_actor( "player_two", Cycle( color = constants.BLUE, position = 355 ) )
-    cast.add_actor( "player_one_score", Player_one_score() )
-    cast.add_actor( "player_two_score", Player_two_score() )
+    player_two = cast.get_first_actor( "player_two" )
+    tail_two = player_two.get_segments()
+    cast.add_actor( "player_two_tail", Tail( color = constants.BLUE, player_segments = tail_two ) )
+    cast.add_actor( "player_one_score", Score( y = 0, name = "Player One" ) )
+    cast.add_actor( "player_two_score", Score( y = 800, name = "Player Two" ) )
    
     # Start the game.
     keyboard_service = KeyboardService()
